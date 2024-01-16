fa69fbd9801fef334c24eb23f755c27f2241e5c5
baArlene <carloschinyuku@gmail.com>
2024-01-17 00:33:04 +0200

    MuiSelect Field +++

--- react-material-ui-app/src/components/MuiSelect.tsx
+++ react-material-ui-app/src/components/MuiSelect.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, MenuItem, TextField } from "@mui/material";
+import { useState } from "react";
 
 const MuiSelect = () => {
-  return (
-    <div>MuiSelect</div>
-  )
-}
+  const [countries, setCountries] = useState<string[]>([]);
+  console.log({ countries });
 
-export default MuiSelect
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setCountries(typeof value === 'string' ? value.split(',') : value);
+  };
+
+  return (
+    <Box width="250px">
+      <TextField
+        label="Select Country"
+        select
+        value={countries}
+        onChange={handleChange}
+        fullWidth
+        SelectProps={{
+            multiple: true
+        }}
+        size="medium"
+        color="secondary"
+        helperText="Please Select your Country"
+        error
+      >
+        <MenuItem value="BW">Botswana</MenuItem>
+        <MenuItem value="ZAR">South Africa</MenuItem>
+        <MenuItem value="ZW">Zimbabwe</MenuItem>
+        <MenuItem value="NM">Namibia</MenuItem>
+      </TextField>
+    </Box>
+  );
+};
+
+export default MuiSelect;
